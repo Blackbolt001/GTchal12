110157686842f13df91058cdebee587c94be7cfd
unknown <Jgossett73@gmail.com>
2022-08-23 03:13:43 -0400

    found schema issues

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
-DROP DATABASE IF EXISTS db_employeetracker;
-CREATE DATABASE db_employeetracker;
-
-USE db_employeetracker;
-DROP TABLE IF EXISTS department;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
 CREATE TABLE department (
   id INT PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(30) 
 );
-DROP TABLE IF EXISTS role;
 CREATE TABLE role (
   id INT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+  INDEX dep_ind (department_id),CONSTRAINT fk_department
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
-DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
   id INT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+role_id UNSIGNED NOT NULL,
+INDEX role_ind (role_id),CONSTRAINT fk_role
+FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+manager_id UNSIGNED NOT NULL,
+INDEX man_ind (manager_id),CONSTRAINT fk_manager
+FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+
 );
