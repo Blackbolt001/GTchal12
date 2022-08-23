aa2e19b9509ef062c03084ad4fb0410be27bc751
unknown <Jgossett73@gmail.com>
2022-08-22 23:53:19 -0400

    error handling before functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const { prompt } = require('inquirer');
-const prompts = require('./lib/prompts.js');
+const prompts = require('lib\prompts.js');
 const functions = require('./lib/functions.js');
 
 const db = mysql.createConnection({
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
 const init = () => {
     showQuestions()
 };
-const preTable = async () => {
+const prepQuestions = async () => {
     const setDepartment = await functions.returnDeptArray(db);
     prompts.questions[4].choices = setDepartment;
     prompts.questions[12].choices = setDepartment;
@@ -35,18 +35,40 @@ setManager.unshift({name:"(none",value:null});
 
 const setManagers = await functions.returnManagersArray(db);
     prompts.questions[11].choices = setManagers;
+}
     
-const showTable = async () => {
-    await preTable()
+const showQuestions = async () => {
+    await prepQuestions()
     const data = await prompt(prompts.questions);
     if(data.responseSelection != -1) {
-    showTable();
-}else
-{
-console.log("closing selection");
-process.exit();
+    questionHandler(data);
+    showQuestions();
 }
+else
+{
+    console.log("closing selection");
+    process.exit();
+}
+}
+const questionHandler = (data) => {
+const responseSelection = data.responseSelection;
+    if(responseSelection < 10) {
+        functions.queries(db,data); 
+} 
+else if (responseSelection >= 10 && responseSelection < 20)
+{ 
+    if(responseSelection == 10) {functions.createDepartment(db,data.addDepartment)
 };
+    if(responseSelection == 11) {functions.insertRole(db,data.addRole,data.setSalary,data.employeeDepartment)
 };
+    if(responseSelection == 12) {functions.insertEmployee(db,data.firstName,data.lastName,data.jobs,data.managers)
+};
+}
+else if (responseSelection>=20 && responseSelection < 30) 
+{
+    if(responseSelection == 20) {functions.updateRoleId(db,data.employees,data.jobs)
+};
+    if(responseSelection ==21) {functions.updateManagerId(db,data.employees,data.managers) };
+}}
 
 init();
